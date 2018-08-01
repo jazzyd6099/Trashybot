f54cca467855685d3d5697e5907de2a729ae382c
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 18:20:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,32 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+function doMagic8BallVoodoo() {
+    var rand = [
+    "Yes",
+    "No.",
+    "It is possible.",
+    "Maybe.",
+    "Eh.",
+    "Nah, come back next time.",
+    "I don't care.",
+    "Maybe not.",
+    "Probably.",
+    "Probably not.",
+    "Nah.",
+    "It's a mystery.",
+    "The world may never know.",
+    "I don't know.",
+    "What?",
+    "I cannot answer that.",
+    "How would I know?",
+    "Ah.. No.",
+    "How the fuck do I answer this?!",
+];
+
+    return rand[Math.floor(Math.random()*rand.length)];
+}
+
 client.on("ready", () => {
   console.log("I am ready to be a trashy ass bot.");
 	
@@ -18,9 +44,12 @@ client.on("ready", () => {
 const prefix = "!";
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix)) return;
-  
+	
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong.");
+	    } else
+	  if (message.content.startsWith(prefix + "give me a nickname")) {
+			message.reply(doMagic8BallVoodoo());
   }
 });
 
