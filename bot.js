04977d2dc6b38c32406cb3afad9c2704a6c8f813
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 23:52:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,12 +75,17 @@ client.on('guildMemberAdd', member => {
   });
 const prefix = "!";
 client.on("message", (message) => {
-  // Exit and stop if it's not there
+
   if (!message.content.startsWith(prefix)) return;
 	
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong.");
 		  } else
+  		  if (message.content.startsWith(prefix + "clear")) {
+  		 	 if (args[0])message.channel.send("Invalid.");
+  				message.channel.bulkDelete(args[0]).then(() => {
+  			  message.channel.send("Cleared ${args[0]} messages.").then(message => message.delete(5000));
+				} else
 		  	if (message.content.startsWith(prefix + "trashtalk")) {
 			let trashtalkedUser = message.mentions.users.first();
 				
