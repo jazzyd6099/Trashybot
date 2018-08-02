2b1f5412db84bb318a2adf073daae91d0937a49e
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 21:30:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,20 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+//embedColors
+
+const embedRed = 0xff0000
+const embedOrange = 0xff790c
+const embedYellow = 0xffff00
+const embedGreen = 0x00ff00
+const embedBlue = 0x0064ff
+const embedPurple = 0x6a00b0
+const embedMagenta = 0x9600ff
+const embedPink = 0xff00ff
+const embedBlack = 0x000000
+const embedWhite = 0xffffff
+const embedGray = 0x777777
+
 client.on("ready", () => {
   console.log("I am ready to be a trashy ass bot.");
 	
@@ -10,7 +24,11 @@ client.on("ready", () => {
   .catch(console.error);
 	});
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('474327704280367154').send("Welcome to RandomShit. If you want to know what I can do, go to the text channel; #bot-commands. Enjoy the discord server and have fun.");
+    member.guild.channels.get('474327704280367154').send({embed: {
+	    				color: embedWhite,
+	    				title: "Welcome to RandomShit.",
+	    				description: "If you want to know what I can do, go to the text channel; #bot-commands. Enjoy the discord server and have fun.",
+    					}});
  });
   client.on("message", (message) => {
 	if (message.content.startsWith("i'm trash")) {
