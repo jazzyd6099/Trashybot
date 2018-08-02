28d047b1f51531327233f08c043e8c1e4b567579
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 23:42:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,6 @@ client.on('guildMemberAdd', member => {
 const prefix = "!";
 client.on("message", (message) => {	
 
- if (message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	
