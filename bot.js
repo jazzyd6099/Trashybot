2259bb78dc7c6794d9885badf8bd42c8e8cc72a9
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 21:19:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,7 @@ client.on("message", (message) => {
 				
             if(message.mentions.users.size < 1) return message.reply("You forgot to mention someone for me to trashtalk.");
             message.channel.send("Fucking bitch, you weirdass hoe. :unamused:" + trashtalkedUser);
+			} else
 	  		if (message.content.startsWith(prefix + "turn back")) {
 		message.guild.member(client.user).setNickname('Trashy Bot');
             client.user.setAvatar("https://cdn.discordapp.com/attachments/474340568474976256/474366295232479232/depositphotos_69083153-stock-photo-steel-trash-can-isolated-on.jpg");
