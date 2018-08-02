bd16942ac8f75693c8c76896bd13e774eb2b9f62
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-08-01 23:46:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
 	
-  if (!message.content.startsWith(prefix + "ping")) {
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong.");
 		  } else
 		  	if (message.content.startsWith(prefix + "trashtalk")) {
